8d616a13fb9324c9cd51c3c2d81df15fe2486ebe
Leokre <73553627+Leokre@users.noreply.github.com>
2021-04-19 13:36:18 +0200

    added Helper method

--- src/pages/Chat/chatContent/ChatContent.js
+++ src/pages/Chat/chatContent/ChatContent.js
@@ -7,6 +7,7 @@ import Axios from "axios"
 import qs from "qs"
 import FunctionBar from "./functionBar/FunctionBar";
 import SendIcon from '@material-ui/icons/Send';
+import {msgTimeFromTimestamp} from "../../../Helpers"
 const backendURL = process.env.REACT_APP_BACKEND_URL
 var i = 1
   
